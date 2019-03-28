c03cfb2fb267006fbc051b2a69d2a795e35f7601
Balwinder Singh <bobby.brot@hotmail.co.uk>
2019-03-28 16:49:56 +0000

    GeneratePrimeNumbers - Final Commit
    -Finalised work on project

--- GeneratePrimeNumbers/main.c
+++ GeneratePrimeNumbers/main.c
@@ -11,20 +11,47 @@ int main()
 {
     /*Variable Declarations*/
     int primes[100] = {2,3}; //Holds prime numbers
-    int arrayIndexer = 2; //Tracks the next index that is free in Primes Array (above)
+    int arrayIndexer = 2; //Tracks the next index that is free and ready to be used in Primes Array (above)
+    int isPrime = 1;
 
-    /*Identify Prime Number Definition*/
-    for(int i = 4; i < 100; i++)
+
+    /*Identify Prime Numbers*/
+    //Iterate through odd numbers as even values will not be primes
+    for(int i = 3; i < 100; i = i + 2)
     {
-        /*Check if even number*/
-        if((i % 2) == 0) //If current loop index is even
+        //Reset isPrime boolean for every next value we will test against
+        isPrime = 1;
+
+        //Access all elements in primes array
+        for(int j = 0; j < arrayIndexer; j++)
         {
-            continue; //Go to next loop cycle
+            //If the current value from outer loop is evenly divisible (value is not a prime number)
+            if((i % primes[j]) == 0)
+            {
+                //Set boolean to false and escape inner loop
+                isPrime = 0;
+                break;
+            }
+        }
+
+        //If the current value is a prime number
+        if(isPrime == 1)
+        {
+            //Store current value in primes array
+            primes[arrayIndexer] = i;
+
+            //Increment arrayIndexer for next loop iteration
+            arrayIndexer++;
         }
 
     }
 
 
     /*Output Primes array*/
+    for(int i = 0; i < arrayIndexer; i++)
+    {
+        printf("%d  ",primes[i]);
+    }
+
     return 0;
 }
