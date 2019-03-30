49652a85a9212acc0486b37d8eb80e3fc4770d1f
Balwinder Singh <bobby.brot@hotmail.co.uk>
2019-03-30 16:59:41 +0000

    Writing Functions, Final Commit
    -Finalised work on code that calculates Square Root

--- Writing Functions/main.c
+++ Writing Functions/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 /*****************************
 * Name:     Balwinder Singh
 * Purpose:  Get familiar with C functions and understand it's
@@ -17,20 +17,88 @@ int main()
 {
     /*Variable Declarations*/
     int inputOne, inputTwo;
+    float squareRInput, squareRResult;
     float GCD;
 
-    /*User Input Handler*/
-    printf("Input 2 values: \n");
-    scanf("%d %d", &inputOne, &inputTwo);
+    /*User Input Handler for GCD*/
+//    printf("Input 2 values: \n");
+//    scanf("%d %d", &inputOne, &inputTwo);
 
-    /*Function Calls*/
-    GCD = gcd(inputOne, inputTwo);
-    printf("\n\nGCD = %.2f",GCD);
+    /*Greater Common Divisor*/
+//    GCD = gcd(inputOne, inputTwo);
+//    printf("\nGCD = %.2f",GCD);
+
+
+    /*Square Root*/
+    printf("\n\nPlease input value to find Square Root for: ");
+    scanf("%f",&squareRInput);
+    squareRResult =squareRoot(squareRInput);
+    printf("\nSquare Root Result: %f",squareRResult);
 
     return 0;
 }
 
 /*Function Definitions*/
+float squareRoot(float input)
+{
+    /*Variable Declarations*/
+    float   squareR;
+    float   temp = 1; //Aid with square root calculation
+    int     unitsChecker = 0; //0 = unit, 1 = uniths, 2 = tenths
+    bool    squareRFound = false;
+
+
+    //Get Absolute Value from helper function
+    float absInput = abs(input);
+
+    /*Square root calculation accurate to 2 decimal places*/
+    while(!squareRFound)
+    {
+        temp = squareR * squareR; //Calculate squared of value
+
+        if(temp == absInput) //If value of temp matches target value
+        {
+            squareRFound = true;
+        }
+        else if (temp < absInput) //If less than value
+        {
+            if(unitsChecker == 0)
+                squareR++; //Increment temp normally
+            else if(unitsChecker == 1)
+                squareR = squareR + .1; //Increment uniths part
+            else if(unitsChecker == 2)
+                squareR = squareR + .01; //Increment tenths part
+        }
+        else if (temp > absInput) //If target value is not evenly squared
+        {
+            if(unitsChecker == 0)
+            {
+                unitsChecker++;
+                squareR--;
+            }
+            else if(unitsChecker == 1)
+            {
+                unitsChecker++;
+                squareR = squareR - .1;
+            }
+            else if(unitsChecker == 2)
+            {
+                squareRFound = true;
+                squareR -= .01;
+            }
+        }
+    }
+
+    return squareR;
+}
+
+
+float absVal(float value)
+{
+    return abs(value);
+}
+
+
 float gcd(int inputOne, int inputTwo)
 {
     /*Variable Declarations*/
@@ -38,7 +106,6 @@ float gcd(int inputOne, int inputTwo)
     int inputOneR; //Holds remainder
     int inputTwoR; //Holds remainder
     int lowestValue;
-
 
     /*Find Greatest Common Divisor*/
     //Tenary if statement that records lowest value from user input
