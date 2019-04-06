fb807fa6bfd76a168d1ba380c5b1e69b2969a841
Balwinder Singh <bobby.brot@hotmail.co.uk>
2019-04-06 15:16:13 +0100

    Structure Pointers and Functions, First Commit
    -Program that tests my understanding of pointers, structures and DMA (Dynamic Memory Allocation) to pass and work with structures through different functions.
    -Program takes input from user for item name, quantity, price. It then calculates the amount and then prints out the data from structure pointer to screen.

---
+++ Structure Pointers and Functions/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+/***********************************
+* Name:     Balwinder Singh
+* Purpose:  Program that uses structures and pointers to pass
+*           structure data into functions
+***********************************/
+
+struct item
+{
+    char*   itemName;
+    int     quantity;
+    float   price;
+    float   amount; //Quantity * Price
+};
+
+
+//
+void readItem(struct item *);
+void printItem(struct item *);
+
+int main()
+{
+
+    //Allocates memory and stores into ptr
+    struct item *ptr = (struct item*)malloc(sizeof(struct item));
+
+    //Allocate memory for pointer in structure
+    ptr->itemName = (char*)malloc(50 *sizeof(char));
+    if(ptr == 0 || ptr->itemName == 0) //Check memory has been allocated successfully
+    {
+        printf("MEMORY COULD NOT BE ASSIGNED");
+        return 1;
+    }
+
+    readItem(ptr);
+
+    return 0;
+}
+
+void printItem(struct item *ptr)
+{
+    printf("\n'''PRODUCT INFORMATION'''\n");
+
+    printf("Item Name:  %s\n",ptr->itemName);
+    printf("Quantity:   %d\n",ptr->quantity);
+    printf("Price:      %.2f\n",ptr->price);
+    printf("Amount:     %.2f",ptr->amount);
+
+}
+
+void readItem(struct item *ptr)
+{
+    printf("Item Name: ");
+    scanf("%[^\n]s",ptr->itemName);
+
+    printf("Quantity: ");
+    scanf("%d",&ptr->quantity);
+
+    printf("Price: ");
+    scanf("%f",&ptr->price);
+
+    ptr->amount = ptr->quantity * ptr->price;
+
+    printItem(ptr);
+
+}
